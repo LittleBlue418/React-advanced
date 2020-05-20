a2e506c2306470c16d4c82a33dbfa1a72684a74e
LittleBlue418 <hollyathomas88@gmail.com>
2020-05-20 15:26:13 +0200

    Commenting out console.log lifecycle tracking

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Person from './Person/Person'
 
 const persons = (props) => {
-    console.log('[Persons.js] render');
+    //console.log('[Persons.js] render');
     return props.persons.map((person, index) => {
     return (
         <Person
--- src/containers/App.js
+++ src/containers/App.js
@@ -8,11 +8,11 @@ import LetterCards from '../components/LetterCards/LetterCards';
 
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    console.log('[App.js] constructor');
-    // this.state = (state below) <- another way of setting state
-  }
+  // constructor(props) {
+  //   super(props);
+  //   console.log('[App.js] constructor');
+  //   // this.state = (state below) <- another way of setting state
+  // }
 
   state = {
     persons: [
@@ -26,14 +26,14 @@ class App extends Component {
     userTextArray: {},
   };
 
-  static getDerivedStateFromProps(props, state) {
-    console.log('[App.js] getDerivedStateFromProps', props);
-    return state;
-  }
+  // static getDerivedStateFromProps(props, state) {
+  //   console.log('[App.js] getDerivedStateFromProps', props);
+  //   return state;
+  // }
 
-  componentDidMount () {
-    console.log('[App.js] componentDidMount')
-  }
+  // componentDidMount () {
+  //   console.log('[App.js] componentDidMount')
+  // }
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
@@ -99,7 +99,7 @@ class App extends Component {
   }
 
   render() {
-    console.log('[App.js] render')
+    //console.log('[App.js] render')
     let persons = null;
     let letterCards = null;
 
