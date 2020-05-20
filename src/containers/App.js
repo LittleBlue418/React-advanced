46de75b6596f140e20c03e9632962bc840583a19
LittleBlue418 <hollyathomas88@gmail.com>
2020-05-20 10:10:33 +0200

    simplifying Persons rendering function

--- src/containers/App.js
+++ src/containers/App.js
@@ -89,13 +89,10 @@ class App extends Component {
 
     if (this.state.showPersons) {
       persons = (
-        <div>
           <Persons
             persons={this.state.persons}
             clicked={this.deletePersonHandler}
-            changed={this.nameChangedHandler}
-          />
-        </div>
+            changed={this.nameChangedHandler} />
       );
     }
 
