41c892b71fcf4342f17552b5afc80db59d729244
LittleBlue418 <hollyathomas88@gmail.com>
2020-06-01 20:35:22 +0200

    Fragment

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import Auxiliary from '../../../hoc/Auxiliary';
+import React, { Component, Fragment } from 'react';
+// import Auxiliary from '../../../hoc/Auxiliary';
 import classes from './Person.module.css';
 
 
@@ -7,14 +7,14 @@ class Person extends Component {
   render() {
     //console.log('[Person.js] rendering...')
     return (
-      <Auxiliary>
+      <Fragment>
         <div key="div" className={classes.Person}>
           < p onClick={this.props.click} > I'm {this.props.name} and I am {this.props.age} years old!</p>
           < p > {this.props.children}</p >
           <input type="text" onChange={this.props.changed} value={this.props.name} />
         </div>
         <p key="heading">-</p>
-      </Auxiliary>
+      </Fragment>
     )
   }
 };
