e894d0b4575f816d8dedc2daabab181dc48b3e9d
LittleBlue418 <hollyathomas88@gmail.com>
2020-06-01 13:07:53 +0200

    using a higher order component to return adjacent

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
+import Auxiliary from '../../../hoc/Auxiliary';
 import classes from './Person.module.css';
 
 
 class Person extends Component {
   render() {
     //console.log('[Person.js] rendering...')
-    return [
-      <div key="div" className={classes.Person}>
-        < p onClick = { this.props.click } > I'm {this.props.name} and I am {this.props.age} years old!</p>
-        < p > { this.props.children }</p >
+    return (
+      <Auxiliary>
+        <div key="div" className={classes.Person}>
+          < p onClick={this.props.click} > I'm {this.props.name} and I am {this.props.age} years old!</p>
+          < p > {this.props.children}</p >
           <input type="text" onChange={this.props.changed} value={this.props.name} />
-      </div>,
-      <p key="heading">-</p>
-    ]
+        </div>
+        <p key="heading">-</p>
+      </Auxiliary>
+    )
   }
 };
 
