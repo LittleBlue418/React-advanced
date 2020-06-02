fb62890cc818b96ec4ca8b7c36df146dbb1cc3e8
LittleBlue418 <hollyathomas88@gmail.com>
2020-06-02 10:33:24 +0200

    passing unknown components with spread

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,22 +1,25 @@
 import React, { Component, Fragment } from 'react';
-// import Auxiliary from '../../../hoc/Auxiliary';
+import Auxiliary from '../../../hoc/Auxiliary';
 import classes from './Person.module.css';
+import withClass2 from '../../../hoc/WithClass2';
 
 
 class Person extends Component {
   render() {
     //console.log('[Person.js] rendering...')
     return (
-      <Fragment>
-        <div key="div" className={classes.Person}>
+      //<Fragment>
+      <Auxiliary>
+
           < p onClick={this.props.click} > I'm {this.props.name} and I am {this.props.age} years old!</p>
           < p > {this.props.children}</p >
           <input type="text" onChange={this.props.changed} value={this.props.name} />
-        </div>
+
         <p key="heading">-</p>
-      </Fragment>
+      </Auxiliary>
+      //</Fragment>
     )
   }
 };
 
-export default Person;
+export default withClass2(Person, classes.Person);
